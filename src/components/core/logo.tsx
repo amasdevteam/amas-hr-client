12bf5618b6856920d7f9a52136c072777c9f435a
ppnathaniel <patricianphillip@gmail.com>
2025-03-28 10:49:33 -0500

    wip

--- src/components/core/logo.tsx
+++ src/components/core/logo.tsx
@@ -22,9 +22,9 @@ export function Logo({ color = "dark", emblem, height = HEIGHT, width = WIDTH }:
 	let url: string;
 
 	if (emblem) {
-		url = color === "light" ? "/assets/logo-emblem.svg" : "/assets/logo-emblem--dark.svg";
+		url = color === "light" ? "/assets/logo-emblem1.svg" : "/assets/logo-emblem--dark1.svg";
 	} else {
-		url = color === "light" ? "/assets/logo.svg" : "/assets/logo--dark.svg";
+		url = color === "light" ? "/assets/logo1.svg" : "/assets/logo--dark1.svg";
 	}
 
 	return <Box alt="logo" component="img" height={height} src={url} width={width} />;
